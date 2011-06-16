e401d64c46a91fa9d2c3f63b9736feffbfbfef0b
jramoyo <jramoyo@c29f06c5-f593-60b5-31af-ed673223a778>
2011-06-16 11:20:14 +0000

    Added "Help" menu

---
+++ qfix-messenger/src/com/jramoyo/qfixmessenger/ui/listeners/HelpActionListener.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2011, Jan Amoyo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright 
+ *   notice, this list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer 
+ *   in the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ *
+ * HelpActionListener.java
+ * 16 Jun 2011
+ */
+package com.jramoyo.qfixmessenger.ui.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jramoyo.qfixmessenger.ui.QFixMessengerFrame;
+
+/**
+ * @author jamoyo
+ */
+public class HelpActionListener implements ActionListener
+{
+	private static final Logger logger = LoggerFactory
+			.getLogger(HelpActionListener.class);
+
+	private QFixMessengerFrame frame;
+
+	public HelpActionListener(QFixMessengerFrame frame)
+	{
+		this.frame = frame;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		try
+		{
+			java.awt.Desktop.getDesktop().browse(
+					java.net.URI.create(frame.getMessenger().getConfig()
+							.getHelpURL()));
+		} catch (IOException ex)
+		{
+			logger.error("An exception occured!", ex);
+			JOptionPane.showMessageDialog(frame, "An exception occured:\n"
+					+ Arrays.toString(ex.getStackTrace()), "Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+}
