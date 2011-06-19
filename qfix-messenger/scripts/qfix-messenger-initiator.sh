0a4eff46ee6d7679f47c96fa0073823bebf09d50
jramoyo <jramoyo@c29f06c5-f593-60b5-31af-ed673223a778>
2011-06-19 03:52:26 +0000

    Added acceptor and initiator scripts

---
+++ qfix-messenger/scripts/qfix-messenger-initiator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CLASSPATH=cfg/initiator
+CLASSPATH=$CLASSPATH:lib/qfix-messenger.jar
+CLASSPATH=$CLASSPATH:lib/jdom.jar
+CLASSPATH=$CLASSPATH:lib/log4j-1.2.16.jar
+CLASSPATH=$CLASSPATH:lib/mina-core-1.1.7.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-core-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix40-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix41-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix42-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix43-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix44-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix50-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fixt11-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-api-1.6.1.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-log4j12-1.6.1.jar
+
+java -cp "$CLASSPATH" com.jramoyo.qfixmessenger.QFixMessenger "cfg/initiator/messenger.cfg" "cfg/initiator/quickfix.cfg"
