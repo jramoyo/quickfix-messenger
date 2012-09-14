479441524e9a687eca25cc948c3bb53d0686180d
jramoyo <jramoyo@c29f06c5-f593-60b5-31af-ed673223a778>
2012-09-14 14:07:35 +0000

    Issue 25: Upgrade to QuickFIX/J 1.5.2

--- qfix-messenger/scripts/qfix-messenger-acceptor.bat
+++ qfix-messenger/scripts/qfix-messenger-acceptor.bat
@@ -10,14 +10,7 @@ set CLASSPATH=%CLASSPATH%;lib\jdom.jar
 set CLASSPATH=%CLASSPATH%;lib\lipstikLF-1.1.jar
 set CLASSPATH=%CLASSPATH%;lib\log4j-1.2.16.jar
 set CLASSPATH=%CLASSPATH%;lib\mina-core-1.1.7.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-core-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix40-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix41-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix42-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix43-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix44-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fix50-SNAPSHOT.jar
-set CLASSPATH=%CLASSPATH%;lib\quickfixj-msg-fixt11-SNAPSHOT.jar
+set CLASSPATH=%CLASSPATH%;lib\quickfixj-all-1.5.2.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-api-1.6.1.jar
 set CLASSPATH=%CLASSPATH%;lib\slf4j-log4j12-1.6.1.jar
 
--- qfix-messenger/scripts/qfix-messenger-initiator.sh
+++ qfix-messenger/scripts/qfix-messenger-initiator.sh
@@ -6,14 +6,7 @@ CLASSPATH=$CLASSPATH:lib/jdom.jar
 CLASSPATH=$CLASSPATH:lib/lipstikLF-1.1.jar
 CLASSPATH=$CLASSPATH:lib/log4j-1.2.16.jar
 CLASSPATH=$CLASSPATH:lib/mina-core-1.1.7.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-core-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix40-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix41-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix42-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix43-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix44-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fix50-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:lib/quickfixj-msg-fixt11-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:lib/quickfixj-all-1.5.2.jar
 CLASSPATH=$CLASSPATH:lib/slf4j-api-1.6.1.jar
 CLASSPATH=$CLASSPATH:lib/slf4j-log4j12-1.6.1.jar
 
